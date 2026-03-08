88c65e00e4b84939f5158225194153438a0fed65
G-Sneha08 <sneha901932@gmail.com>
2026-03-08 14:09:03 +0000

    fewchan

--- seed.js
+++ seed.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 
 // Railway DB connection
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.crossover.proxy.rlwy.net,
+    user: process.env.root,
+    password: process.env.VgknVczRXKYYnbRoIVWRGwMwjOFgJfko,
+    database: process.env.railway,
+    port: process.env.12849
 });
 
 async function seed() {
