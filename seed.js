c6f58b4199b80a319a961a5aae3f50bacb5e94bd
G-Sneha08 <sneha901932@gmail.com>
2026-03-08 14:17:45 +0000

    seed

--- seed.js
+++ seed.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-// Railway DB connection
 const pool = mysql.createPool({
-    host: process.env.mysql.railway.internal,
-    user: process.env.root,
-    password: process.env.VgknVczRXKYYnbRoIVWRGwMwjOFgJfko,
-    database: process.env.railway,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
 
