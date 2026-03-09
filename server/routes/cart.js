633358a0a909e51a35eab4dbcb72a698017257e1
G-Sneha08 <sneha901932@gmail.com>
2026-03-09 05:10:56 +0000

    sillycha

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,4 +1,4 @@
-cconst express = require('express');
+const express = require('express');
 const router = express.Router();
 const pool = require('../config/db');
 const { protect } = require('../middleware/authMiddleware');
