a67438cee92573d3fb440832c50f92a4621b4236
G-Sneha08 <sneha901932@gmail.com>
2026-03-14 18:21:49 +0000

    portchanged

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 5000,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'restaurant',
