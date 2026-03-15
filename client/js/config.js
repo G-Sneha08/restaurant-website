5ae537d51736dcbefeec60a024ac68ddc61c2d6f
G-Sneha08 <sneha901932@gmail.com>
2026-03-15 14:48:35 +0000

    samesss

--- client/js/config.js
+++ client/js/config.js
@@ -1,31 +1,7 @@
-// client/js/config.js
-
-// Detect if running locally or deployed
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
     : "https://restaurant-backend-cli2.onrender.com/api";
 
-
-
-// Global API helper
-async function apiRequest(endpoint, options = {}) {
-
-    const res = await fetch(API_BASE_URL + endpoint, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        ...options
-    });
-
-    if (!res.ok) {
-        throw new Error(`API Error: ${res.status}`);
-    }
-
-    return res.json();
-}
-
-// expose globally
 window.API_BASE_URL = API_BASE_URL;
 window.USER_ID = 1;
-console.log("[CONFIG] API_BASE_URL:", API_BASE_URL);
